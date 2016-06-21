89347df3f519f2940ea5f7893add9091b002459d
Jeremy Evans <code@jeremyevans.net>
2016-06-21 09:38:44 -0700

    Switch to using message_bus

--- config.ru
+++ config.ru
@@ -1,8 +1,2 @@
 require './lila_shell'
-use Rack::Lint
-class Rack::Lint::HijackWrapper
-  def to_int
-    @io.to_i
-  end
-end
 run LilaShell.freeze.app
--- lila_shell.rb
+++ lila_shell.rb
@@ -3,61 +3,23 @@ require 'tilt/erubis'
 require './models'
 require 'json'
 
+require 'message_bus'
+#MessageBus.configure(:backend=>:postgres, :backend_options=>{:user=>'message_bus', :dbname=>'message_bus'})
+MessageBus.configure(:backend=>:memory)
+
+#MessageBus.subscribe "/room/1" do |msg|
+#  p [:subscribe, msg]
+#end
+
 class LilaShell < Roda
-  MUTEX = Mutex.new
-  LISTENERS = {}
-  Thread.new do
-    DB.listen('room', :loop=>true) do |_,_,data|
-begin
-      room_id = JSON.parse(data)['room_id']
-      listeners = MUTEX.synchronize{(LISTENERS[room_id] || []).dup}
-      listeners.each do |ws|
-        ws.send(data)
-      end
-rescue
-    puts e.class, e.message, e.backtrace
-end
-    end
-  end
-
-  if defined?(Reel)
-    class RodaRequest
-      require 'reel/websocket'
-      class WebSocket < ::Reel::WebSocket
-        def self.websocket?(env)
-          ::WebSocket::Driver.websocket?(env) && !!env['reel.request']
-        end
-
-        def initialize(env, options={})
-          @ws = env['reel.request'].websocket
-        end
-
-        def on(meth, &block)
-          @ws.public_send("on_#{meth}", &block)
-        end
-
-        def rack_response
-          [ 101, {}, [] ]
-        end
-
-        def send(s)
-          @ws.write(s)
-        end
-      end
-    end
-  else
-    plugin :websockets, :adapter=>:thin
-  end
-
-  def sync
-   MUTEX.synchronize{yield}
-  end
+  use Rack::CommonLogger
 
   plugin :static, %w'/index.html'
   plugin :render, :escape=>true
   plugin :forme
   plugin :symbol_views
   plugin :symbol_matchers
+  plugin :message_bus
 
   plugin :error_handler do |e|
     puts e.class, e.message, e.backtrace
@@ -69,49 +31,57 @@ end
       :manage
     end
 
-    r.post do
-      r.is 'user' do
-        user = User.create(:name=>r['name'].to_s)
-        r.redirect '/'
+    r.get 'message-bus.js' do
+      File.read 'public/message-bus.js'
+    end
+
+    r.post 'user' do
+      user = User.create(:name=>r['name'].to_s)
+      r.redirect '/'
+    end
+
+    r.on 'room' do
+      r.get true do
+        r.redirect "/room/#{r[:room_id]}/#{r[:user_id]}"
       end
 
-      r.is 'room' do
+      r.post true do
         room = Room.create(:name=>r['name'].to_s)
         r.redirect '/'
       end
-    end
 
-    r.get 'join' do
-      user = @user = User.with_pk!(r['user_id'].to_i)
-      room = @room = Room.with_pk!(r['room_id'].to_i)
-      user_id = user.id
-      user_name = user.name
-      room_id = room.id
+      r.on ":d/:d" do |room_id, user_id|
+        @user = User.with_pk!(user_id.to_i)
+        @room = Room.with_pk!(room_id.to_i)
+        @channel = "/room/#{@room.id}"
+        
+        next "No access" if @user.name == @room.name
 
-      r.websocket do |ws|
-        sync{(LISTENERS[room_id] ||= []) << ws}
-        data = {:join=>user_name, :room_id=>room_id, :at=>Time.now.strftime('%H:%M:%S')}.to_json
-        DB.notify('room', :payload=>data)
+        r.message_bus(@channel)
 
-        ws.on :message do |event|
-          str = event.is_a?(String) ? event : event.data
-          next if str.empty?
-
-          DB.transaction do
-            m = Message.create(:user_id=>user_id, :room_id=>room_id, :message=>str)
-            data = {:user=>user_name, :room_id=>room_id, :message=>str, :at=>m.at.strftime('%H:%M:%S')}.to_json
-            DB.notify('room', :payload=>data)
-          end
+        r.get true do
+          :room
         end
 
-        ws.on :close do |event|
-          sync{(LISTENERS[room_id] || []).delete(ws)}
-          data = {:leave=>user_name, :room_id=>room_id, :at=>Time.now.strftime('%H:%M:%S')}.to_json
-          DB.notify('room', :payload=>data)
+        r.post "join" do
+          MessageBus.publish(@channel, {:join=>@user.name, :at=>Time.now.strftime('%H:%M:%S')}.to_json)
+          ''
+        end
+
+        r.post "leave" do
+          MessageBus.publish(@channel, {:leave=>@user.name, :at=>Time.now.strftime('%H:%M:%S')}.to_json)
+          ''
+        end
+
+        r.post "message" do
+          post = r[:post].to_s.strip
+          unless post.empty?
+            m = Message.create(:user_id=>@user.id, :room_id=>@room.id, :message=>post)
+            MessageBus.publish(@channel, {:user=>m.user.name, :room_id=>@room.id, :message=>m.message, :at=>m.at.strftime('%H:%M:%S')}.to_json)
+          end
+          ''
         end
       end
-
-      :room
     end
   end
 end
