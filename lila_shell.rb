7f7a20bbbcb1e85e15957d43b01d559e0e2a471b
Jeremy Evans <code@jeremyevans.net>
2018-03-09 16:28:13 -0800

    Switch to using typecast_params plugin

--- lila_shell.rb
+++ lila_shell.rb
@@ -19,6 +19,9 @@ class App < Roda
   plugin :symbol_views
   plugin :symbol_matchers
   plugin :message_bus, :message_bus=>MESSAGE_BUS
+  plugin :request_aref, :raise
+  plugin :typecast_params
+  alias tp typecast_params
 
   plugin :error_handler do |e|
     puts e.class, e.message, e.backtrace
@@ -35,18 +38,18 @@ class App < Roda
     end
 
     r.post 'user' do
-      user = User.create(:name=>r['name'].to_s)
+      user = User.create(:name=>tp.nonempty_str!('name'))
       r.redirect '/'
     end
 
     r.on 'room' do
       r.is do
         r.get do
-          r.redirect "/room/#{r[:room_id]}/#{r[:user_id]}"
+          r.redirect "/room/#{tp.pos_int!('room_id')}/#{tp.pos_int!('user_id')}"
         end
 
         r.post do
-          room = Room.create(:name=>r['name'].to_s)
+          room = Room.create(:name=>tp.nonempty_str!('name'))
           r.redirect '/'
         end
       end
@@ -75,7 +78,7 @@ class App < Roda
         end
 
         r.post "message" do
-          post = r[:post].to_s.strip
+          post = tp.str!('post').strip
           unless post.empty?
             m = Message.create(:user_id=>@user.id, :room_id=>@room.id, :message=>post)
             MESSAGE_BUS.publish(@channel, {:user=>m.user.name, :room_id=>@room.id, :message=>m.message, :at=>m.at.strftime('%H:%M:%S')}.to_json)
