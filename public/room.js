a46209c030ef93e2f6925a08751af0c4b520cbbb
Jeremy Evans <code@jeremyevans.net>
2021-12-08 10:19:45 -0800

    Stop using jQuery
    Switch to message-bus-ajax.js and some custom javascript.
    Update message-bus.js while here.
    
    Disable autocomplete on the post input.

--- public/room.js
+++ public/room.js
@@ -1,59 +1,71 @@
 (function() {
-  var post = $('#post');
-  var posts = $('#posts');
-  var content = $('#content');
-  var csrfs = $('#post_form');
-  var csrf_field = csrfs.data('field');
-  var baseUrl = csrfs.data('base-url');
-
-  var resize = function() {
-    $('#content').css('height', window.innerHeight-120);
-  }
+  var post = document.getElementById('post');
+  var posts = document.getElementById('posts');
+  var content = document.getElementById('content');
+  var csrfs = document.getElementById('post_form');
+  var csrf_field = csrfs.getAttribute('data-field');
+  var baseUrl = csrfs.getAttribute('data-base-url');
 
   var jumpToPageBottom = function() {
-    content.scrollTop(content.prop("scrollHeight"));
+    content.scrollTop = content.scrollHeight;
+  }
+
+  var resize = function() {
+    content.style.height = (window.innerHeight - 85) + 'px';
+    jumpToPageBottom();
   }
 
   var add_line = function(s){
-    var li = $("<li></li>");
-    li.text(s);
-    posts.append(li);
+    var li = document.createElement('li');
+    li.innerText = s;
+    posts.appendChild(li);
     jumpToPageBottom();
   }
 
   resize();
-  $(window).resize(resize);
-  jumpToPageBottom();
+  window.onresize = resize;
   post.focus();
 
   MessageBus.baseUrl = baseUrl;
   MessageBus.start();
-  MessageBus.subscribe(csrfs.data('channel'), function(data){
+  MessageBus.subscribe(csrfs.getAttribute('data-channel'), function(data){
     data = JSON.parse(data);
+    var time = '<' + data.at + '> ';
     if (data.join) {
-      add_line('<' + data.at + '> ' + data.join + ' joined');
+      add_line(time + data.join + ' joined');
     } else if (data.leave) {
-      add_line('<' + data.at + '> ' + data.leave + ' left');
+      add_line(time + data.leave + ' left');
     } else {
-      add_line('<' + data.at + '> ' + data.user + ': ' + data.message);
+      add_line(time + data.user + ': ' + data.message);
     }
   });
 
-  var join_data = {};
-  join_data[csrf_field] = csrfs.data('join');
-  $.post(baseUrl+"join", join_data);
+  serialize = function(obj) {
+    var str = [];
+    for (var p in obj) {
+      if (obj.hasOwnProperty(p)) {
+        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+      }
+    }
+    return str.join("&");
+  }
+  var submit_post = function(type, data) {
+    data[csrf_field] = csrfs.getAttribute('data-' + type);
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', baseUrl+type);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(serialize(data));
+    return xhr;
+  }
+  submit_post('join', {});
 
-  var leave_data = {};
-  leave_data[csrf_field] = csrfs.data('leave');
   window.onbeforeunload = function(){
-    $.post(baseUrl+"leave", leave_data);
+    submit_post('leave', {});
   };
 
-  $('#post_form').submit(function(e){
-    var post_data = {"post": post.val()};
-    post_data[csrf_field] = csrfs.data('message');
-    $.post(baseUrl+"message", post_data);
-    post.val('');
+  csrfs.onsubmit = function(e){
+    submit_post('message', {"post": post.value});
+    post.value = '';
     e.preventDefault();
-  });
+  };
 })();
